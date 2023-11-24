9aeae1cd35ee82cf3d9f7fb1f5f7fc1e879b97bd
gopi ananda naidu <gopianandakumar@gmail.com>
2023-11-24 23:44:55 +0530

    created first input taking boxes for multiple inputs we have to add css for this

--- room_maintenance/src/Components/Inputcard.jsx
+++ room_maintenance/src/Components/Inputcard.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Inputcard() {
-  return <div>Inputcard</div>;
+  const [val, setVal] = useState([]);
+
+  const handleAdd = () => {
+    const abc = [...val, []];
+    setVal(abc);
+  };
+
+  const handleChange = (onChangeValue, i) => {
+    const inputData = [...val];
+    inputData[i] = onChangeValue.target.value;
+    setVal(inputData);
+  };
+
+  const handleDelete = (i) => {
+    const deleteData = [...val];
+    deleteData.splice(i, 1);
+    setVal(deleteData);
+  };
+
+  console.log(val, "dddddddddddddddd");
+
+  return (
+    <>
+      <button onClick={() => handleAdd()}>Add</button>
+
+      {val.map((data, i) => {
+        return (
+          <div>
+            <input value={data} onChange={(e) => handleChange(e, i)} />
+            <button onClick={() => handleDelete(i)}>X</button>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Inputcard;
